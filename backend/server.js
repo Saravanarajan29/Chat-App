7606b5a92509b9a31d667a846fd93df449ffce2d
Saravanarajan29 <saravanarajansr29@gmail.com>
2024-04-10 00:40:52 +0530

    Chat App completed

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,26 @@
-import express from  "express";
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes  from './routes/auth.routes.js';
-import messageRoutes  from './routes/message.routes.js';
-import userRoutes from  './routes/user.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongoDb.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json()); // Middleware - to  parse the incoming requests with JSON payloads (from req.body) and return responses in JSON format
-app.use(cookieParser()); //  parse cookies
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-
-app.listen(PORT,() => {
-    connectToMongoDB();
-    console.log(`server Running on port ${PORT}`);
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
